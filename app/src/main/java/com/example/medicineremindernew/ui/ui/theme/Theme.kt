69a19fc5b3ea0e464d4ec4e11b5197bf109e11ac
VanRoberto~ <143705646+IvanRoberto83@users.noreply.github.com>
2025-07-24 19:25:04 +0700

    theme adjust

--- app/src/main/java/com/example/medicineremindernew/ui/ui/theme/Theme.kt
+++ app/src/main/java/com/example/medicineremindernew/ui/ui/theme/Theme.kt
@@ -8,18 +8,19 @@ package com.example.medicineremindernew.ui.ui.theme
     import androidx.compose.material3.dynamicLightColorScheme
     import androidx.compose.material3.lightColorScheme
     import androidx.compose.runtime.Composable
+    import androidx.compose.ui.graphics.Color
     import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = BiruTua,
+    secondary = BiruMuda,
+    background = Color.Black
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = BiruTua,
+    secondary = BiruMuda,
+    background = Color.White
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -42,11 +43,11 @@ fun MedicineReminderNewTheme(
     val colorScheme = when {
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
             val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+            if (darkTheme) dynamicDarkColorScheme(context) else dynamicDarkColorScheme(context)
         }
 
         darkTheme -> DarkColorScheme
-        else -> LightColorScheme
+        else -> DarkColorScheme
     }
 
     MaterialTheme(
